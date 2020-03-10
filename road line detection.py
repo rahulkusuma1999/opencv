4902a67f015b8029a6a628f449605e9a6ac80a74
rahulkusuma1999 <55886415+rahulkusuma1999@users.noreply.github.com>
2020-03-10 19:23:22 +0530

    Add files via upload

---
+++ road line detection.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+image= cv2.imread('road.jpg')
+image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+
+
+height= image.shape[0]
+width=image.shape[1]
+
+
+region_of_intrest =
+
